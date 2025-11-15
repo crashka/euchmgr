ecc87069cc471b7554310e0e95c2b3fbe8b7fca0
crash <ltung783@gmail.com>
2025-11-15 15:16:52 -0800

    add script files for demo/testing

---
+++ run_demo.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+TOURN="${TOURN:-nola_2025}"
+ROSTER="${ROSTER:-nola_2025_roster.csv}"
+
+python -m euchmgr ${TOURN} tourn_create force=t
+echo "Tournament \"${TOURN}\" created"
+read -p "Press any key to upload roster..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} upload_roster ${ROSTER}
+echo -n "Generating player nums..."
+echo "done"
+python -m euchmgr ${TOURN} generate_player_nums
+echo -n "Building seeding bracket..."
+echo "done"
+python -m euchmgr ${TOURN} build_seed_bracket
+read -p "Press any key to create fake seeding results..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} fake_seed_results
+echo -n "Tabulating seeding results..."
+echo "done"
+python -m euchmgr ${TOURN} tabulate_seed_round
+echo -n "Computing player rankings..."
+echo "done"
+python -m euchmgr ${TOURN} compute_player_seeds
+read -p "Press any key to create fake partner picks..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} fake_partner_picks
+echo -n "Building tournament teams..."
+echo "done"
+python -m euchmgr ${TOURN} build_tourn_teams
+echo -n "Computing tournament team seeds..."
+echo "done"
+python -m euchmgr ${TOURN} compute_team_seeds
+echo -n "Building tournament brackets..."
+echo "done"
+python -m euchmgr ${TOURN} build_tourn_bracket
+read -p "Press any key to create fake tournament results..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} fake_tourn_results
+echo -n "Tabulating tournament results..."
+echo "done"
+python -m euchmgr ${TOURN} tabulate_tourn
+echo -n "Computing tournament rankings..."
+echo "done"
+python -m euchmgr ${TOURN} compute_team_ranks
---
+++ run_steps.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+TOURN="${TOURN:-nola_2025}"
+ROSTER="${ROSTER:-nola_2025_roster.csv}"
+
+python -m euchmgr ${TOURN} tourn_create force=t
+echo "Tournament \"${TOURN}\" created"
+read -p "Press any key to upload roster..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} upload_roster ${ROSTER}
+read -p "Press any key to generate player nums..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} generate_player_nums
+read -p "Press any key to build seeding bracket..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} build_seed_bracket
+read -p "Press any key to create fake seeding results..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} fake_seed_results
+read -p "Press any key to tabulate seeding results..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} tabulate_seed_round
+read -p "Press any key to compute player rankings..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} compute_player_seeds
+read -p "Press any key to create fake partner picks..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} fake_partner_picks
+read -p "Press any key to build tournament teams..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} build_tourn_teams
+read -p "Press any key to compute tournament team seeds..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} compute_team_seeds
+read -p "Press any key to build tournament brackets..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} build_tourn_bracket
+read -p "Press any key to create fake tournament results..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} fake_tourn_results
+read -p "Press any key to tabulate tournament results..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} tabulate_tourn
+read -p "Press any key to compute tournament rankings..." -n1 -s
+echo "done"
+python -m euchmgr ${TOURN} compute_team_ranks

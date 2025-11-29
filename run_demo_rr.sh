d16a65659a094a7825b1b2f35c24c3968d1104bb
crash <ltung783@gmail.com>
2025-11-29 12:08:51 -0800

    add a couple more demo scripts (for round robin live dashboard)

---
+++ run_demo_rr.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+TOURN="${TOURN:-nola_2025}"
+ROSTER="${ROSTER:-nola_2025_roster.csv}"
+
+nteams=20
+nrounds=8
+
+LIMIT=${1:-10}
+LOOPS=${2:-$(((nteams / 2 * nrounds - 1) / LIMIT + 1))}
+
+echo "LIMIT = " ${LIMIT}
+echo "LOOPS = " ${LOOPS}
+
+python -m euchmgr "${TOURN}" tourn_create force=t
+echo "Tournament \"${TOURN}\" created"
+read -p "Press any key to upload roster..." -n1 -s
+echo "done"
+python -m euchmgr "${TOURN}" upload_roster "${ROSTER}"
+echo -n "Generating player nums..."
+echo "done"
+python -m euchmgr "${TOURN}" generate_player_nums
+echo -n "Building seeding bracket..."
+echo "done"
+python -m euchmgr "${TOURN}" build_seed_bracket
+read -p "Press any key to create fake seeding results..." -n1 -s
+echo "done"
+python -m euchmgr "${TOURN}" fake_seed_games
+echo -n "Validating seeding results..."
+echo "done"
+python -m euchmgr "${TOURN}" validate_seed_round
+echo -n "Computing player rankings..."
+echo "done"
+python -m euchmgr "${TOURN}" compute_player_seeds
+echo -n "Prepicking champ partners..."
+echo "done"
+python -m euchmgr "${TOURN}" prepick_champ_partners
+read -p "Press any key to create fake partner picks..." -n1 -s
+echo "done"
+python -m euchmgr "${TOURN}" fake_pick_partners
+echo -n "Building tournament teams..."
+echo "done"
+python -m euchmgr "${TOURN}" build_tourn_teams
+echo -n "Computing tournament team seeds..."
+echo "done"
+python -m euchmgr "${TOURN}" compute_team_seeds
+echo -n "Building tournament brackets..."
+echo "done"
+python -m euchmgr "${TOURN}" build_tourn_bracket
+
+for i in $(seq 1 ${LOOPS}) ; do
+    read -p "Press any key to create ${LIMIT} fake tournament results..." -n1 -s
+    echo "done"
+    python -m euchmgr "${TOURN}" fake_tourn_games limit=${LIMIT}
+done
+
+echo -n "Validating tournament results..."
+echo "done"
+python -m euchmgr "${TOURN}" validate_tourn
+echo -n "Computing tournament rankings..."
+echo "done"
+python -m euchmgr "${TOURN}" compute_team_ranks
---
+++ run_test_rr.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+TOURN="${TOURN:-nola_2025}"
+ROSTER="${ROSTER:-nola_2025_roster.csv}"
+
+nteams=20
+nrounds=8
+
+LIMIT=${1:-10}
+LOOPS=${2:-$(((nteams / 2 * nrounds - 1) / LIMIT + 1))}
+SLEEP=${3:-5}
+
+echo "LIMIT = " ${LIMIT}
+echo "LOOPS = " ${LOOPS}
+echo "SLEEP = " ${SLEEP}
+
+python -m euchmgr "${TOURN}" tourn_create force=t
+echo "Tournament \"${TOURN}\" created"
+echo -n "Uploading roster..."
+echo "done"
+python -m euchmgr "${TOURN}" upload_roster "${ROSTER}"
+echo -n "Generating player nums..."
+echo "done"
+python -m euchmgr "${TOURN}" generate_player_nums
+echo -n "Building seeding bracket..."
+echo "done"
+python -m euchmgr "${TOURN}" build_seed_bracket
+echo -n "Creating fake seeding results..."
+echo "done"
+python -m euchmgr "${TOURN}" fake_seed_games
+echo -n "Validating seeding results..."
+echo "done"
+python -m euchmgr "${TOURN}" validate_seed_round
+echo -n "Computing player rankings..."
+echo "done"
+python -m euchmgr "${TOURN}" compute_player_seeds
+echo -n "Prepicking champ partners..."
+echo "done"
+python -m euchmgr "${TOURN}" prepick_champ_partners
+echo -n "Creating fake partner picks..."
+echo "done"
+python -m euchmgr "${TOURN}" fake_pick_partners
+echo -n "Building tournament teams..."
+echo "done"
+python -m euchmgr "${TOURN}" build_tourn_teams
+echo -n "Computing tournament team seeds..."
+echo "done"
+python -m euchmgr "${TOURN}" compute_team_seeds
+echo -n "Building tournament brackets..."
+echo "done"
+python -m euchmgr "${TOURN}" build_tourn_bracket
+
+for i in $(seq 1 ${LOOPS}) ; do
+    echo -n "Creating ${LIMIT} fake tournament results..."
+    echo "done"
+    python -m euchmgr "${TOURN}" fake_tourn_games limit=${LIMIT}
+    sleep $SLEEP
+done
+
+echo -n "Validating tournament results..."
+echo "done"
+python -m euchmgr "${TOURN}" validate_tourn
+echo -n "Computing tournament rankings..."
+echo "done"
+python -m euchmgr "${TOURN}" compute_team_ranks

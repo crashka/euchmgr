9004ceb6a3b403b3af36f67514aeb955d470fcb8
crash <ltung783@gmail.com>
2026-01-05 20:11:56 -0800

    move common mobile test stuff into `conftest.py`, initial test for post_score stuff (more to come)

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,19 @@ import shutil
 
 import pytest
 from peewee import SqliteDatabase
+from flask import Flask
+from flask.testing import FlaskClient
 
 from core import TEST_DIR
 from database import db_filepath, db_init, db_close, db_reset
 from schema import TournStage, clear_schema_cache
+from server import Config, create_app
 
-#############
-# Constants #
-#############
+##################
+# database stuff #
+##################
 
 TEST_DB = "test"
-
-#################
-# Utility Funcs #
-#################
 
 def stage_db_path(stage_num: int) -> str:
     """Build full pathname for stage-level database snapshot.
@@ -47,10 +46,6 @@ def restore_stage_db(stage: TournStage) -> SqliteDatabase:
     shutil.copy2(stage_db_path(stage), db_filepath(TEST_DB))
     db = db_init(TEST_DB, force=True)
     return db
-
-############
-# Fixtures #
-############
 
 @pytest.fixture
 def stage_1_db() -> Generator[SqliteDatabase]:
@@ -189,3 +184,56 @@ def tourn_bracket_db() -> Generator[SqliteDatabase]:
     yield db
     db_close()
     clear_schema_cache()
+
+################
+# mobile stuff #
+################
+
+TEST_USER_AGENT = "Mobile test client"
+
+class FlaskTestClientProxy(object):
+    """From https://stackoverflow.com/q/15278285
+    """
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ['HTTP_USER_AGENT'] = TEST_USER_AGENT
+        return self.app(environ, start_response)
+
+class TestConfig(Config):
+    """Subclass the default flask app config
+    """
+    pass
+
+@pytest.fixture(scope="module")
+def seed_bracket_app(seed_bracket_db) -> Generator[Flask]:
+    """Module-level app instantiation (caches database reference)
+    """
+    app = create_app(TestConfig)
+    app.wsgi_app = FlaskTestClientProxy(app.wsgi_app)
+    app.testing = True
+    yield app
+
+@pytest.fixture(scope="module")
+def tourn_bracket_app(tourn_bracket_db) -> Generator[Flask]:
+    """Module-level app instantiation (caches database reference)
+    """
+    app = create_app(TestConfig)
+    app.wsgi_app = FlaskTestClientProxy(app.wsgi_app)
+    app.testing = True
+    yield app
+
+@pytest.fixture()
+def client(seed_bracket_app):
+    """Unauthenticated client instance
+    """
+    app = seed_bracket_app
+    yield app.test_client()
+
+def get_user_client(app: Flask, user: str) -> FlaskClient:
+    """Fake fixture, return authenticated client instance
+    """
+    client = app.test_client()
+    client.post("/login", data={'username': user}, follow_redirects=True)
+    return client
--- tests/test_mobile.py
+++ tests/test_mobile.py
@@ -2,87 +2,48 @@
 
 """Test team ranking process, including tie-breaking logic.
 """
-from collections.abc import Generator
-
 import pytest
-from flask import Flask
-from flask.testing import FlaskClient
 
-from server import Config, create_app
-
-################
-# client proxy #
-################
-
-TEST_USER_AGENT = "Mobile test client"
-
-class FlaskTestClientProxy(object):
-    """From https://stackoverflow.com/q/15278285
-    """
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        environ['HTTP_USER_AGENT'] = TEST_USER_AGENT
-        return self.app(environ, start_response)
-
-##########
-# config #
-##########
-
-class TestConfig(Config):
-    """Subclass the default flask app config
-    """
-    pass
-
-############
-# fixtures #
-############
-
-@pytest.fixture(scope="module")
-def app(seed_bracket_db) -> Generator[Flask]:
-    """Module-level app instantiation (caches database reference)
-    """
-    app = create_app(TestConfig)
-    app.wsgi_app = FlaskTestClientProxy(app.wsgi_app)
-    app.testing = True
-    yield app
-
-@pytest.fixture()
-def client(app):
-    """Unauthenticated client instance
-    """
-    yield app.test_client()
-
-def get_user_client(app: Flask, user: str) -> FlaskClient:
-    """Fake fixture, return authenticated client instance
-    """
-    client = app.test_client()
-    client.post("/login", data={'username': user}, follow_redirects=True)
-    return client
-
-#########
-# tests #
-#########
+from conftest import get_user_client
 
 def test_index(client):
     """Sanity check, hitting '/' should return login page
     """
-    response = client.get('/', follow_redirects=True)
-    assert response.status_code == 200
-    assert len(response.history) == 1
-    assert response.request.path == '/login'
-    assert "Select player..." in response.text
+    resp = client.get('/', follow_redirects=True)
+    assert resp.status_code == 200
+    assert len(resp.history) == 1
+    assert resp.request.path == '/login'
+    assert "Select player..." in resp.text
 
-def test_logins(app):
+def test_logins(seed_bracket_app):
     """Make sure disparate client sessions are independent of each other
     """
+    app = seed_bracket_app
     crash_client = get_user_client(app, "Crash")
-    response = crash_client.get('/mobile', follow_redirects=True)
-    assert response.status_code == 200
-    assert "Logged in as: <span>Crash</span>" in response.text
+    resp = crash_client.get('/mobile/', follow_redirects=True)
+    assert resp.status_code == 200
+    assert "Logged in as: <span>Crash</span>" in resp.text
 
     abs_client = get_user_client(app, "Abs")
-    response = abs_client.get('/mobile', follow_redirects=True)
-    assert response.status_code == 200
-    assert "Logged in as: <span>Abs</span>" in response.text
+    resp = abs_client.get('/mobile/', follow_redirects=True)
+    assert resp.status_code == 200
+    assert "Logged in as: <span>Abs</span>" in resp.text
+
+def test_post_score(seed_bracket_app):
+    """Make sure disparate client sessions are independent of each other
+    """
+    app = seed_bracket_app
+    virgilio_client = get_user_client(app, "Virgilio")
+    resp = virgilio_client.get('/mobile/', follow_redirects=True)
+    assert resp.status_code == 200
+    assert "Logged in as: <span>Virgilio</span>" in resp.text
+
+    data = {'game_label'   : 'sd-1-1',
+            'posted_by_num': '1',
+            'team_idx'     : '0',
+            'ref_score_id' : 'None',
+            'team_pts'     : '10',
+            'opp_pts'      : '0',
+            'action'       : 'submit_score'}
+    resp = virgilio_client.post('/mobile/', data=data, follow_redirects=True)
+    assert "<label>Score posted by</label>: <span>Virgilio (you)</span>" in resp.text
